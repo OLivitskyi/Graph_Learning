0dd4fc7a2cd88c299f3b37db168a70e99485b596
oleg_livitskyi <olivitskyi@gmail.com>
2024-07-20 21:32:02 +0300

    Fixed paths for GitHub Pages deployment

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -8,7 +8,6 @@ interface User {
   id: number;
   login: string;
   email: string;
-  name: string
 }
 
 const GET_USER_INFO = gql`
@@ -17,7 +16,6 @@ const GET_USER_INFO = gql`
       id
       login
       email
-      name
     }
   }
 `;
@@ -35,7 +33,7 @@ const MainPageContent: React.FC = () => {
   const user = data?.user[0]; 
   return (
     <div className="main-page">
-      <h1>Welcome, {user?.name}</h1>
+      <h1>Welcome, {user?.login}</h1>
       <p>User ID: {user?.id}</p>
       <p>Email: {user?.email}</p>
       <Graphs />
